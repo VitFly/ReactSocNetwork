70377a7276b1e1982528050031e66a7c87bbf3ba
VitFly <tommy.babcook@gmail.com>
2021-04-27 20:54:34 +0300

    2stC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = ()  => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img  src ='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/mr-bread-logo-design-template-8a8bf7264deccf537f8dd318785ebd6f_screen.jpg?ts=1579209248' alt=""/ >
       </header>
-    </div>
+      <nav className='nav'>
+          <div><a href> Profile</a> </div>
+          <div><a href> Messages</a> </div> 
+          <div><a href> News</a> </div> 
+          <div><a href> Music</a> </div> 
+          <div><a href> Settings</a> </div> 
+      </nav>
+      <div className='content' >
+          <div>
+             <img src ='https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg' alt=''/>
+          </div>
+          <div>
+            ava + description
+          </div>
+          <div>
+            My posts
+            <div>
+              New post
+            </div>
+              <div>
+                <div>
+                  post1
+                </div>
+                <div>
+                  post 2
+                </div>
+              </div>
+          </div>
+      </div>
+    </div> 
   );
 }
+
 
 export default App;
